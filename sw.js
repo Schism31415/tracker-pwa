26f0e3dabb2b6086cc6952c1f15cdc5810d62552
Schism31415 <edmundsnick1@gmail.com>
2025-10-02 16:30:51 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,37 +8,27 @@ const FILES_TO_CACHE = [
   "./icons/icon-512.png"
 ];
 
-// Install SW and cache files
+// Install SW
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
 
-// Activate SW and clear old caches
+// Activate SW
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
+      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
     )
   );
   self.clients.claim();
 });
 
-// Fetch from cache or network
+// Fetch
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
